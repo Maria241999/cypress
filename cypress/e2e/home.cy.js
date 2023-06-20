761133b54501e7fd24037c4f9ed5be8b82f7d0f3
Maria241999 <mariacf9918@gmail.com>
2023-06-19 20:41:48 -0600

    Version1
    Clonacion

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('first test', () => {
+    cy.visit('http://localhost:3000')
+  })
+  it('second test', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('third test', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
